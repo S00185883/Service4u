42851f19ad5664851a1381d1d6fcca2da754c66e
Tara McDonagh <47326811+TaraMcDonagh@users.noreply.github.com>
2022-04-10 22:12:12 +0100

    authenticaiton added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
 import "./App.css";
 
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./Home/Home";
 import ProviderList from "./Provider/ProviderList";
 import Provider from "./Provider/provider";
 import Booking from "./Booking/booking";
+import Login from "./User/Login";
+import Register from "./User/Register";
+import Reset from "./User/Reset";
+import Dashboard from "./User/Dashboard";
 
 
 
@@ -13,12 +17,16 @@ class App extends Component {
   render() {
     return (
       <Router>
-        <Switch>
+        <Routes>
           <Route path="/" exact={true} component={Home} />
           <Route path="/providers" exact={true} component={ProviderList} />
           <Route path="/provider/:id" component={Provider} />
           <Route path="/booking/:id" component={Booking} />
-        </Switch>
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/register" element={<Register />} />
+          <Route exact path="/reset" element={<Reset />} />
+          <Route exact path="/dashboard" element={<Dashboard />} />
+        </Routes>
       </Router>
     );
   }
---
+++ src/User/firebase.js
@@ -0,0 +1,96 @@
+import { initializeApp } from "firebase/app";
+import {
+  GoogleAuthProvider,
+  getAuth,
+  signInWithPopup,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  sendPasswordResetEmail,
+  signOut,
+} from "firebase/auth";
+import {
+  getFirestore,
+  query,
+  getDocs,
+  collection,
+  where,
+  addDoc,
+} from "firebase/firestore";
+const firebaseConfig = {
+  apiKey: "AIzaSyAM-zIYfRLQXyOvoe2dYHhPKxOrMdrsvT4",
+  authDomain: "project400-332015.firebaseapp.com",
+  databaseURL:
+    "https://project400-332015-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "project400-332015",
+  storageBucket: "project400-332015.appspot.com",
+  messagingSenderId: "802691143590",
+  appId: "1:802691143590:web:743b03be9c2b2f52396167",
+  measurementId: "G-TKJLY6WMQY",
+};
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const googleProvider = new GoogleAuthProvider();
+const signInWithGoogle = async () => {
+  try {
+    const res = await signInWithPopup(auth, googleProvider);
+    const user = res.user;
+    const q = query(collection(db, "users"), where("uid", "==", user.uid));
+    const docs = await getDocs(q);
+    if (docs.docs.length === 0) {
+      await addDoc(collection(db, "users"), {
+        uid: user.uid,
+        name: user.displayName,
+        authProvider: "google",
+        email: user.email,
+      });
+    }
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+const logInWithEmailAndPassword = async (email, password) => {
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+const registerWithEmailAndPassword = async (name, email, password) => {
+  try {
+    const res = await createUserWithEmailAndPassword(auth, email, password);
+    const user = res.user;
+    await addDoc(collection(db, "users"), {
+      uid: user.uid,
+      name,
+      authProvider: "local",
+      email,
+    });
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+const sendPasswordReset = async (email) => {
+  try {
+    await sendPasswordResetEmail(auth, email);
+    alert("Password reset link sent!");
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+const logout = () => {
+  signOut(auth);
+};
+export {
+  auth,
+  db,
+  signInWithGoogle,
+  logInWithEmailAndPassword,
+  registerWithEmailAndPassword,
+  sendPasswordReset,
+  logout,
+};
